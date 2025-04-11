2dfe7930b7cc595d17ad38a37cf15bafce0a7969
nirvanajoshi <damodarj36@gmail.com>
2025-04-11 11:45:54 +0545

    Create printf.java
    Use of printf in java for formated output

---
+++ printf.java
@@ -0,0 +1,12 @@
+
+class printf {
+    public static void main(String[] args) {
+        String name = "Damodar Joshi";
+        int age = 20;
+        double height = 180.5;
+        
+   System.out.printf("Name:%s \n",name);
+   System.out.printf("Age:%d years \n",age);
+   System.out.printf("Height:%f cm \n",height);
+}
+}
