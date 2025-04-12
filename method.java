19261b62b6b7962d32b052e47cba4fd6a69f909a
nirvanajoshi <damodarj36@gmail.com>
2025-04-12 15:12:27 +0545

    Create method.java
    simple  method use

---
+++ method.java
@@ -0,0 +1,20 @@
+class method {
+     static void happybirthday(String name,int age){
+            
+            System.out.println("Happy birthy to you "+name);
+            System.out.println("You are now "+age+" year old.");
+            System.out.println("You will get gift today.");
+            System.out.println("Happy birthy to you.");
+            
+        }
+    public static void main(String[] args) { 
+        String name = "Damodar joshi";
+        int age = 20;
+        
+        happybirthday(name,age);
+        happybirthday(name,age);
+        happybirthday(name,age);
+        happybirthday(name,age);
+        
+    }
+   }
