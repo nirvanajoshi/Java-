eab64333ca9220b6455a8d5a1a5d9a02158990e4
nirvanajoshi <damodarj36@gmail.com>
2025-04-13 13:03:38 +0545

    Create quizgame.java

---
+++ quizgame.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+class quizgame{
+    // program to print number dail
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+     //Quetions
+     String[] qns ={
+          "What is the function of ROM?",
+          "What is the brain of Computer?",
+          "Who is the the father of Computer? ",
+          "When was Facce book launched?",
+          "What is the first programming language?",
+          };
+    String[] ans ={"storage","cpu","charles babbage","2004","c"};
+    String[] result = new String[5];
+    System.out.println("**************************************");
+    System.out.println("***** Welcome to quiz game ***********");
+    System.out.println("**************************************");
+           for(int i =0;i<5;i++){
+               System.out.println("**************************************");
+               System.out.println(qns[i]);
+               result[i]=input.nextLine();
+               System.out.println("**************************************");
+              
+           }
+           
+    int corr = 0;
+      for(int i =0;i<5;i++){
+         if(result[i].equalsIgnoreCase(ans[i])){
+             corr++;
+         }
+        System.out.println("**************************************");
+        System.out.println("**************************************");
+      }
+      
+      System.out.println("Correct answer:"+corr);
+      input.close();
+               
+        }
+     
+    }
